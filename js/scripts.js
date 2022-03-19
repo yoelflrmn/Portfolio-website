eb775f3f1832b7c4fec02acf6d3a54961d2834dd
yoelflorimon <101151011+yoelflrmn@users.noreply.github.com>
2022-03-19 12:00:06 -0500

    my first "js" code

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,1 @@
-function menuToggle() {
-    var x = document.getElementById('myNavtoggle');
-    if (x.className === 'navtoggle') {
-      x.className += ' responsive';
-    } else {
-      x.className = 'navtoggle';
-    }
-  }
+console.log("Hey! Welcome to My Portfolio Site");
