bd98b4272205763a0b5e79cfabb948fb0c49e1db
yoelflorimon <101151011+yoelflrmn@users.noreply.github.com>
2022-03-22 15:52:11 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,9 @@
-console.log("Hey! Welcome to My Portfolio Site");
+ anime({
+    targets: "#designer path",
+    strokeDashoffset: [anime.setDashoffset, 0],
+    easing: "easeInOutSine",
+    duration: 900,
+    delay: function(el, i) { return i * 150 },
+    direction: "alternate",
+    loop: true,
+  });
